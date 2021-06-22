b6ea8c2def13079233d4b350d666d859e7767999
connuh <cnriscute@gmail.com>
2021-06-22 12:41:00 +0000

    🎉 + Add User schema

---
+++ src/schemas/User.js
@@ -0,0 +1,54 @@
+/**
+ * @author Conner
+ * @since 22/06/21
+ */
+
+// Dependencies
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  discriminator: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  usernames: {
+    type: Array,
+    required: true
+  },
+  avatars: {
+    type: Array,
+    required: true
+  },
+  guilds: {
+    type: Array,
+    required: true
+  },
+  presences: {
+    type: Array,
+    required: true
+  },
+  last_seen: {
+    type: Number,
+    required: true
+  }
+}, {
+  versionKey: false
+});
+
+export default mongoose.model("users", UserSchema);
